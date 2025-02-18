e0897c04260025741adafea292b17b1337eac5df
Cjay94 <cjephthah94@gmail.com>
2025-02-18 16:54:03 +0200

    Created Product Controller and Route 100% working with DB

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+
 /* ROUTE IMPORTS */
 import dashboardRoutes from "./routes/dashboardRoutes";
+import productRoutes from "./routes/productRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -20,6 +22,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
+app.use("/products", productRoutes); // http://localhost:8000/products
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
